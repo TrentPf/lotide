1fb9c9e53b1385ffe2f79ced6cf9ca7e637a4b1c
Trent Pfeiffer <trentpf@hotmail.com>
2021-10-27 13:54:26 +0000

    Add comments

--- middle.js
+++ middle.js
@@ -23,16 +23,16 @@ const assertArraysEqual = function(array1, array2) {
 
 const middle = function(array) {
   let middleArray = [];
-  if (array.length < 3) {
+  if (array.length < 3) { //return middleArray without pushing if array is not long enough
     return middleArray;
   }
 
-  let middleIndex = Math.floor(array.length / 2);
+  let middleIndex = Math.floor(array.length / 2); //Middle index of 8 is 4 (plus index 3 for both middle values), middle index of 7 is 3.5 -> 3
 
   if (array.length % 2 === 0) {
-    middleArray.push(array[middleIndex - 1], array[middleIndex]);
+    middleArray.push(array[middleIndex - 1], array[middleIndex]); //middleIndex - 1 is the left middle value, middleIndex is the right middle value
   } else {
-    middleArray.push(array[middleIndex]);
+    middleArray.push(array[middleIndex]); //only one middle for odd length arrays
   }
   return middleArray;
 };
